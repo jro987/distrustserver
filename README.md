4650fb0bca1594cc9a7dfe14304809a57156728c
jro987 <jnjurow@gmail.com>
2020-03-03 20:36:49 -0800

    finish with js to update stats

---
+++ README.md
@@ -0,0 +1,1 @@
+# Distrust Server
---
+++ source/javascripts/site.js
@@ -0,0 +1,33 @@
+// This is where it all goes :)
+window.onload = function () {
+    var num = Math.floor(Math.random() * 3 + 1);
+    if (num === 1) {
+        $('#header-cont').addClass('plane-bg');
+    } else if (num === 2) {
+        $('#header-cont').addClass('swamp-bg');
+    } else {
+        $('#header-cont').addClass('scientist-bg');
+    }
+    $('#header-cont').addClass();
+    $.ajax({
+        url: "https://rust-servers.net/api/?object=servers&element=detail&key=rCYO3GfTjhXIVmaKTVYtWQCBhMLe2pTwIRH",
+        type: "GET",
+        success: function (result) {
+            data = JSON.parse(result);
+            console.log(result);
+            $('#name').text(data.hostname);
+            $('#status').text(data.is_online);
+            $('#uptime').text(data.uptime + '% Uptime');
+            $('#players').text(data.players + ' / ' + data.maxplayers + ' Players');
+            $('#address').text('45.92.39.41:28025');
+            if (data.is_online === '1') {
+                $('#status').html('The Server is <span class="online">Online</span>');
+            } else {
+                $('#status').html('The Server is <span class="offline">Offline</span>');
+            }
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+};
